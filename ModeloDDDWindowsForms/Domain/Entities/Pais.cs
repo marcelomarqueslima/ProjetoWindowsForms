b305b730317b64c149ebea837a5748c0eb5342b6
Marcelo Marques <38513182+marcelomarkes@users.noreply.github.com>
2020-03-14 00:16:52 -0300

    Projeto inicial
    Projeto inicial windowsforms

---
+++ ModeloDDDWindowsForms/Domain/Entities/Pais.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Pais : IIdentityEntity
+    {
+        public int PaisId { get; set; }
+        public string DescricaoPais { get; set; }
+    }
+}
